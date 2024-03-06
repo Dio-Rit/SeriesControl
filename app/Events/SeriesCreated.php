f01da85ae634364a3cc2f5190e9eacb57b60e528
Dio-Rit <dionatan.ritter@universo.univates.br>
2024-03-06 15:57:45 -0300

    Confirmando commits

--- app/Events/SeriesCreated.php
+++ app/Events/SeriesCreated.php
@@ -10,6 +10,8 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
+/* teste*/
+
 class SeriesCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
